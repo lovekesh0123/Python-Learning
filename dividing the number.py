e1a4b8ed9bcff165b607e452e4d7f1f1acd8646c
lovekesh0123 <117276680+lovekesh0123@users.noreply.github.com>
2022-11-03 17:49:38 +0530

    Add files via upload

---
+++ dividing the number.py
@@ -0,0 +1,6 @@
+#write a program to read two numbers and print their quotient and remainder
+a=int(input("enter the dividend:"))#to take input as integer
+b=int(input("enter the divisor:"))
+q=a//b
+r=a%b
+print(f"qoutient is {q} and remainder is {r}")
