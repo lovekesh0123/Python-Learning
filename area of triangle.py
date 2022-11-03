01f100b3629da6af12980f99e953a04b1df56633
lovekesh0123 <117276680+lovekesh0123@users.noreply.github.com>
2022-11-03 17:47:20 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,5 @@
+#write a program to find the area of a triangle using formula 1/2 * base * height
+a=int(input('ENTER THE HEIGHT OF THE TRIANGLE = '))
+b=int(input('ENTER THE BASE OF THE TRIANGLE = '))
+c=(1/2)*(b*a)
+print(f'THE AREA OF THE TRIANGLE = {c}')
