e421d4fb7a43c147304e642b013b9b7bc5173519
lovekesh0123 <117276680+lovekesh0123@users.noreply.github.com>
2022-11-05 13:48:28 +0530

    Add files via upload

---
+++ goats and hens.py
@@ -0,0 +1,6 @@
+H=int(input('Enter the no. of heads in the farm= '))
+L=int(input('Enter the no. of legs in the farm= '))
+g=(L-(2*H))//2
+h=H-g
+print(f'the number of goats = {g}')
+print(f'the number of hens = {h}')
